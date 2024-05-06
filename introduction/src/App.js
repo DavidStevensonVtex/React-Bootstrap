80d385b682184643c613e74383c32b4ba6f15407
David Stevenson <drsgis@rit.edu>
2024-05-06 16:44:40 -0400

    Example with Badge and Button

--- introduction/src/App.js
+++ introduction/src/App.js
@@ -1,19 +1,11 @@
 import Container from 'react-bootstrap/Container';
-import ExampleToast from './components/ExampleToast' ;
+import Example from './components/Example' ;
 
 import './App.css';
 
 const App = () => (
   <Container className="p-3">
-    <Container className="p-5 mb-4 bg-light rounded-3">
-      <h1 className="header">Welcome To React-Bootstrap</h1>
-      <ExampleToast>
-        We now have Toasts
-        <span role="img" aria-label="tada">
-          🎉
-        </span>
-      </ExampleToast>
-    </Container>
+    <Example />
   </Container>
 );
 
