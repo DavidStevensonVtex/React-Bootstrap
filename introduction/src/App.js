beecac59c31bba6f864d754cf4a611b722d67c9e
David Stevenson <drsgis@rit.edu>
2024-05-06 16:41:35 -0400

    Basic Example

--- introduction/src/App.js
+++ introduction/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import Container from 'react-bootstrap/Container';
+import ExampleToast from './components/ExampleToast' ;
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Container className="p-3">
+    <Container className="p-5 mb-4 bg-light rounded-3">
+      <h1 className="header">Welcome To React-Bootstrap</h1>
+      <ExampleToast>
+        We now have Toasts
+        <span role="img" aria-label="tada">
+          🎉
+        </span>
+      </ExampleToast>
+    </Container>
+  </Container>
+);
 
 export default App;
