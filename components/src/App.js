6e59d262487e15581ba75a60daff602fccdfcbe7
David Stevenson <drsgis@rit.edu>
2024-05-17 09:26:36 -0400

    Components: List Groups: Horizontal List Groups with Breakpoints
    There are responsive variants to horizontal: setting it to {sm|md|lg|xl|xxl} makes the list group horizontal starting at that breakpoint’s min-width.

--- components/src/App.js
+++ components/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import HorizontalExample from "./ListGroups/HorizontalExample" ;
+import HorizontalResponsiveExample from "./ListGroups/HorizontalResponsiveExample" ;
 
 function App() {
   return (
-    <HorizontalExample />
+    <HorizontalResponsiveExample />
   );
 }
 
