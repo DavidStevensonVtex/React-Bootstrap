d9ae5a9d80fe0e6edd5ee5102d4efeab33c95ba0
David Stevenson <drsgis@rit.edu>
2024-05-17 10:08:26 -0400

    Components: Modals: Vertically centered
    You can vertically center a modal by passing the centered prop.

--- components/src/App.js
+++ components/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
-import WithoutAnimationExample from "./Modals/WithoutAnimationExample" ;
+import React from 'react';
+import MyVerticallyCenteredModal from "./Modals/MyVerticallyCenteredModal" ;
+import Button from 'react-bootstrap/Button';
 
 function App() {
+  const [modalShow, setModalShow] = React.useState(false);
+
   return (
-    <WithoutAnimationExample />
+    <>
+      <Button variant="primary" onClick={() => setModalShow(true)}>
+        Launch vertically centered modal
+      </Button>
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </>
   );
 }
+
+// render(<App />);
 
 export default App;
