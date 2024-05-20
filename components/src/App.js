cb2c040fc08e6afb8c103ca0f630eb92e239a5f2
David Stevenson <drsgis@rit.edu>
2024-05-20 14:38:38 -0400

    Components: Navbars
    A powerful, responsive navigation header, the navbar. Includes support for branding, navigation, and more.

--- components/src/App.js
+++ components/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import SizingModalsExample from "./Modals/SizingModalsExample" ;
+import BasicExample from "./Navbars/BasicExample" ;
 
 function App() {
-  return <SizingModalsExample />
+  return <BasicExample />
 }
 
 export default App;
