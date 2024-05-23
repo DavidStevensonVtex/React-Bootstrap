571bc83b48c7872a4011e8a42d2df5bdf0522c86
David Stevenson <drsgis@rit.edu>
2024-05-23 11:52:33 -0400

    Components: Tab.Container, Tab.Content, Tab.Pane, Nav links with Pills

--- components/src/App.js
+++ components/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import JustifiedExample from "./Tabs/JustifiedExample" ;
+import LeftTabsExample from "./Tabs/LeftTabsExample" ;
 
 function App() {
-  return <JustifiedExample />
+  return <LeftTabsExample />
 }
 
 export default App;
